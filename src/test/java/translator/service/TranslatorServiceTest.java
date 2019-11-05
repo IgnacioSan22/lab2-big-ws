19fffdad3bead395cf82454ba4a9c3ddc6e5e1e4
GALVE CEAMANOS, IGNACIO <735508@celes.unizar.es>
2019-11-05 22:26:58 +0100

    Prueba de traducción EN-ES y la traducción obtenida vuelta al original para probar coherencia

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -11,6 +11,8 @@ import translator.domain.TranslatedText;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.Console;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -23,6 +25,18 @@ public class TranslatorServiceTest {
   public void translateTest() {
     TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
     assertEquals("Esto es una prueba de servicio de traducción", translatedText.getTranslation());
+  }
+
+  /**
+   * Test to ensure de coherence between translations into different languages.
+   */
+  @Test
+  public void translateTestCoherence() {
+    TranslatedText translatedText = translatorService.translate("en", "es", "GitHub is free and simple");
+    String spanish = translatedText.getTranslation();
+    assertEquals("GitHub es gratis y sencillo", spanish);
+    TranslatedText translatedText2 = translatorService.translate("es", "en", spanish);
+    assertEquals("GitHub is free and simple", translatedText2.getTranslation());
   }
 
 }
